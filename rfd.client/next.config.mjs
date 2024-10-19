dbc25752bbe53fe63a1e1bf27b186dfbccd27a38
Osku Sirpoma <osku.sirpoma@vartas.fi>
2024-10-19 10:25:13 +0300

    Converted Vite to next + React app to next.js

---
+++ rfd.client/next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+import { env } from 'process';
+
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7127';
+
+const nextConfig = {
+    distDir: './dist', // Changes the build output directory to `./dist/`.
+    rewrites: [
+        {
+            source: '^/weatherforecast',
+            destination: target,
+        },
+    ]
+}
+
+export default nextConfig
